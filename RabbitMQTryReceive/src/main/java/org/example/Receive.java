e929ce6aee3f0d65b7d9907b4bf7fd549acabcc2
Dmitrii Gagarin <gagriu@yandex.ru>
2024-10-21 21:11:52 +0300

    took a look to queues

--- RabbitMQTryReceive/src/main/java/org/example/Receive.java
+++ RabbitMQTryReceive/src/main/java/org/example/Receive.java
@@ -8,22 +8,50 @@ import com.rabbitmq.client.DeliverCallback;
 import java.nio.charset.StandardCharsets;
 
 public class Receive {
-    private final static String QUEUE_NAME = "hello";
+//    private final static String QUEUE_NAME = "hello";
+        private final static String TASK_QUEUE_NAME = "task_queue";
+
 
     public static void main(String[] args) throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
-        Connection connection = factory.newConnection();
-        Channel channel = connection.createChannel();
+        final Connection connection = factory.newConnection();
+        final Channel channel = connection.createChannel();
 
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+//        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+
         System.out.println("Waiting for messages");
+        channel.basicQos(1);
+
+
+//        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+//            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+//            System.out.println("Received: " + message);
+//        };
 
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+            String message = new String(delivery.getBody(), "UTF-8");
             System.out.println("Received: " + message);
+            try {
+                doWork(message);
+            } finally {
+                System.out.println("Work's done");
+                channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+            }
         };
+        channel.basicConsume(TASK_QUEUE_NAME, false, deliverCallback, consumerTag -> { });
+    }
 
-        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> {});
+    private static void doWork(String task) {
+        for (char ch : task.toCharArray()) {
+            if (ch == '.') {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException _ignored) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
     }
 }
--- RabitMQTry/src/main/java/org/example/Send.java
+++ RabitMQTry/src/main/java/org/example/Send.java
@@ -3,22 +3,39 @@ package org.example;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.MessageProperties;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Send {
-    private final static String QUEUE_NAME = "hello";
+//    private final static String QUEUE_NAME = "hello";
+        private final static String TASK_QUEUE_NAME = "task_queue";
+
 
     public static void main(String[] args) throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         try (Connection connection = factory.newConnection();
              Channel channel = connection.createChannel()) {
-            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-            for (int i = 0; i < 10; i++) {
-                String message = String.valueOf(i);
-                channel.basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
-                System.out.print("[x] sent: " + message);
+            channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+//            for (int i = 0; i < 10; i++) {
+//                String message = String.valueOf(i);
+//                channel.basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
+//                System.out.print("[x] sent: " + message);
+//            }
+            ArrayList<String> messages = new ArrayList<String>();
+            messages.add("Hello");
+            messages.add("Hello.");
+            messages.add("Hello..");
+            messages.add("Hello...");
+            messages.add("Hello....");
+            for (String message : messages) {
+                channel.basicPublish("", TASK_QUEUE_NAME,
+                        MessageProperties.PERSISTENT_TEXT_PLAIN,
+                        message.getBytes("UTF-8"));
+                System.out.println("Sent: " + message);
             }
         }
     }
